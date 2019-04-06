9ad8e6fb809e39705066ecebe1cebce969ecddf9
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2019-04-06 13:16:25 +0300

    Translate maxmind

--- upload/install/language/ru-ru/3rd_party/maxmind.php
+++ upload/install/language/ru-ru/3rd_party/maxmind.php
@@ -3,19 +3,19 @@
 $_['heading_title']       = 'MaxMind';
 
 // Text
-$_['text_maxmind']        = 'Fraud detection service.';
-$_['text_success']        = 'MaxMind fraud service has been installed';
-$_['text_signup']         = 'If you don\'t have a license key you can sign up <a href="https://www.maxmind.com/?rId=opencart" target="_blank">here</a>.';
+$_['text_maxmind']        = 'Служба обнаружения мошенничества.';
+$_['text_success']        = 'Служба мошенничества MaxMind установлена';
+$_['text_signup']         = 'Если у вас нет лицензионного ключа, вы можете зарегистрироваться <a href="https://www.maxmind.com/?rId=opencart" target="_blank">здесь</a>.';
 
 // Entry
-$_['entry_key']          = 'License Key';
-$_['entry_score']        = 'Risk score';
-$_['entry_order_status'] = 'Fraud Order Status';
+$_['entry_key']          = 'Лицензионный ключ';
+$_['entry_score']        = 'Оценка риска';
+$_['entry_order_status'] = 'Статус заказа мошенничества';
 
 // Help
-$_['help_score']         = 'The higher the score the more likely the order is fraudulent. Set a score between 0 - 100.';
-$_['help_order_status']  = 'Orders over your set score will be assigned this order status and will not be allowed to reach the complete status automatically.';
+$_['help_score']         = 'Чем выше оценка, тем более вероятно, что заказ является мошенническим. Установите баллы от 0 до 100.';
+$_['help_order_status']  = 'Заказам сверх установленного вами балла будет присвоен этот статус заказа, и он не сможет автоматически получить статуса выполнен.';
 
 // Error
-$_['error_key']          = 'Maxmind licence key required';
-$_['error_score']        = 'A score between 0 and 100 is accepted';
+$_['error_key']          = 'Лицензионный ключ Maxmind обязателен';
+$_['error_score']        = 'Разрешены баллы от 0 до 100';
