072e23bfa5d2472439040c32ea1791af83fdceeb
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2019-04-06 13:14:17 +0300

    Translate footer

--- upload/install/language/ru-ru/common/footer.php
+++ upload/install/language/ru-ru/common/footer.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_project']       = 'Project Homepage';
-$_['text_documentation'] = 'Documentation';
-$_['text_support']       = 'Support Forums';
-$_['text_footer']        = 'Copyright © 2014 OpenCart - All rights reserved';
+$_['text_project']       = 'Официальный сайт';
+$_['text_documentation'] = 'Документация';
+$_['text_support']       = 'Форум поддержки</a> | <a href="https://forum.ruopencart.dev/" target="_blank">Сайт локализации';
+$_['text_footer']        = 'Copyright © 2014 OpenCart - Все права защищены';
