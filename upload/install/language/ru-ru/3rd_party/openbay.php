d148febf1d42293d883ae66503093c1bc4b7825f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2019-04-06 13:16:38 +0300

    Translate openbay

--- upload/install/language/ru-ru/3rd_party/openbay.php
+++ upload/install/language/ru-ru/3rd_party/openbay.php
@@ -3,11 +3,11 @@
 $_['heading_title']      = 'OpenBay Pro';
 
 // Text
-$_['text_openbay']       = 'Multi marketplace integration';
-$_['text_ebay']          = 'eBay is a multi-billion dollar market place that allows business or private sellers to auction and retail goods online. Available to sellers worldwide.';
-$_['text_amazon']        = 'Amazon Marketplace a fixed-price online marketplace allowing sellers to offer new and used items alongside Amazon\'s regular retail service.';
+$_['text_openbay']       = 'Интеграция мульти-магазина';
+$_['text_ebay']          = 'eBay - это многомиллиардный рынок, который позволяет коммерческим или частным продавцам проводить аукционы и розничные продажи товаров через Интернет. Доступно для продавцов по всему миру.';
+$_['text_amazon']        = 'Amazon Marketplace - онлайн-магазин с фиксированной ценой, позволяющий продавцам предлагать новые и подержанные товары наряду с обычным розничным сервисом Amazon.';
 
 // Button
-$_['button_register']    = 'Register';
-$_['button_register_eu'] = 'Register Europe';
-$_['button_register_us'] = 'Register USA';
+$_['button_register']    = 'Регистрация';
+$_['button_register_eu'] = 'Регистрация Европа';
+$_['button_register_us'] = 'Регистрация США';
