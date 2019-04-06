ad5733de9b6ea7449033b6b481bc6f41a6170a13
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2019-04-06 13:16:05 +0300

    Translate install step 4

--- upload/install/language/ru-ru/install/step_4.php
+++ upload/install/language/ru-ru/install/step_4.php
@@ -1,34 +1,34 @@
 <?php
 // Heading
-$_['heading_title']               = 'Installation complete';
+$_['heading_title']               = 'Установка завершена';
 
 // Text
-$_['text_step_4']                 = 'Ready to start selling!';
-$_['text_catalog']                = 'Go to your Online Shop';
-$_['text_admin']                  = 'Login to your Administration';
-$_['text_loading']                = 'Loading modules...';
-$_['text_extension']              = 'visit the extensions store';
-$_['text_mail']                   = 'Join the mailing list';
-$_['text_mail_description']       = 'Stay informed of OpenCart updates and events.';
-$_['text_openbay']                = 'OpenBay Pro gives merchants the ability to link their store with 3rd party markets like eBay, Amazon and Etsy. Import orders, list items and handle shipping information direct from OpenCart...<a href="https://www.openbaypro.com/?utm_source=opencart_install&utm_medium=referral&utm_campaign=opencart_install">More information</a>';
-$_['text_maxmind']                = 'MaxMind provides merchants the ability to identify risky transactions quickly, reducing the risk of fraud and minimises the time spent reviewing orders by giving a risk score for each one...<a href="http://www.maxmind.com/?rId=opencart">More information</a>';
-$_['text_facebook']               = 'Like us on Facebook';
-$_['text_facebook_description']   = 'Tell us how much you like OpenCart!';
-$_['text_facebook_visit']         = 'Visit our Facebook page';
-$_['text_forum']                  = 'Community forums';
-$_['text_forum_description']      = 'Get help from other OpenCart users';
-$_['text_forum_visit']            = 'Visit our forums';
-$_['text_commercial']             = 'Commercial support';
-$_['text_commercial_description'] = 'Development services from OpenCart partners';
-$_['text_commercial_visit']       = 'Visit our partner page';
-$_['text_price']                  = 'Price';
-$_['text_view']                   = 'View details';
-$_['text_download']               = 'Download';
-$_['text_downloads']              = 'Downloads';
+$_['text_step_4']                 = 'Готов для начала продаж!';
+$_['text_catalog']                = 'Перейти к Интернет-Магазину';
+$_['text_admin']                  = 'Войти в Панель управления';
+$_['text_loading']                = 'Загрузка модулей...';
+$_['text_extension']              = 'перейти в магазин расширений';
+$_['text_mail']                   = 'Подписаться на рассылку';
+$_['text_mail_description']       = 'Будьте в курсе обновлений и событий OpenCart.';
+$_['text_openbay']                = 'OpenBay Pro дает продавцам возможность связать свой магазин со сторонними рынками, такими как eBay, Amazon и Etsy. Импортируйте заказы, элементы списка и обрабатывайте информацию о доставке непосредственно из OpenCart... <a href="https://www.openbaypro.com/?utm_source=opencart_install&utm_medium=referral&utm_campaign=opencart_install">Подробнее</a>';
+$_['text_maxmind']                = 'MaxMind предоставляет продавцам возможность быстро выявлять рискованные транзакции, снижая риск мошенничества и сводя к минимуму время, затрачиваемое на рассмотрение заказов, давая оценку риска для каждого из них... <a href="http://www.maxmind.com/?rId=opencart">Дополнительная информация</a>';
+$_['text_facebook']               = 'Поставьте нам лайк на Facebook';
+$_['text_facebook_description']   = 'Расскажите нам, как Вам нравится OpenCart!';
+$_['text_facebook_visit']         = 'Посетите нашу страницу в Facebook';
+$_['text_forum']                  = 'Форум сообщества';
+$_['text_forum_description']      = 'Получить помощь от других пользователей OpenCart';
+$_['text_forum_visit']            = 'Посетите наш форум';
+$_['text_commercial']             = 'Коммерческая поддержка';
+$_['text_commercial_description'] = 'Услуги по разработке от партнеров OpenCart';
+$_['text_commercial_visit']       = 'Посетите страницу наших партнеров';
+$_['text_price']                  = 'Цена';
+$_['text_view']                   = 'Просмотр деталей';
+$_['text_download']               = 'Загрузка';
+$_['text_downloads']              = 'Загрузки';
 
 // Button
-$_['button_mail']                 = 'Join here';
-$_['button_setup']                = 'Set-up now';
+$_['button_mail']                 = 'Подписаться тут';
+$_['button_setup']                = 'Настроить сейчас';
 
 // Error
-$_['error_warning']               = 'Don\'t forget to delete your installation directory!';
+$_['error_warning']               = 'Не забудьте удалить каталог установки: install!';
