e7de443775d1de20637b5bbe04c339d8bcf37264
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2019-04-07 23:48:58 +0300

    Translate catalog\information\sitemap

--- upload/catalog/language/ru-ru/information/sitemap.php
+++ upload/catalog/language/ru-ru/information/sitemap.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'Site Map';
+$_['heading_title']    = 'Карта сайта';
 
 // Text
-$_['text_special']     = 'Special Offers';
-$_['text_account']     = 'My Account';
-$_['text_edit']        = 'Account Information';
-$_['text_password']    = 'Password';
-$_['text_address']     = 'Address Book';
-$_['text_history']     = 'Order History';
-$_['text_download']    = 'Downloads';
-$_['text_cart']        = 'Shopping Cart';
-$_['text_checkout']    = 'Checkout';
-$_['text_search']      = 'Search';
-$_['text_information'] = 'Information';
-$_['text_contact']     = 'Contact Us';
+$_['text_special']     = 'Специальные предложения';
+$_['text_account']     = 'Личный кабинет';
+$_['text_edit']        = 'Данные пользователя';
+$_['text_password']    = 'Пароль';
+$_['text_address']     = 'Список адресов';
+$_['text_history']     = 'История покупок';
+$_['text_download']    = 'Загрузки';
+$_['text_cart']        = 'Корзина';
+$_['text_checkout']    = 'Оформление заказа';
+$_['text_search']      = 'Поиск';
+$_['text_information'] = 'Информация';
+$_['text_contact']     = 'Связаться с нами';
