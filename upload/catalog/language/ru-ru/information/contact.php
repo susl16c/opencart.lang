e5eb24356b4397228ea08463f2e963e594bad50f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2019-04-07 23:48:13 +0300

    Translate catalog\information\contact

--- upload/catalog/language/ru-ru/information/contact.php
+++ upload/catalog/language/ru-ru/information/contact.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']  = 'Contact Us';
+$_['heading_title']  = 'Связаться с нами';
 // Text
-$_['text_location']  = 'Our Location';
-$_['text_store']     = 'Our Stores';
-$_['text_contact']   = 'Contact Form';
-$_['text_address']   = 'Address';
-$_['text_telephone'] = 'Telephone';
-$_['text_fax']       = 'Fax';
-$_['text_open']      = 'Opening Times';
-$_['text_comment']   = 'Comments';
-$_['text_message']   = '<p>Your enquiry has been successfully sent to the store owner!</p>';
+$_['text_location']  = 'Наше место нахождение';
+$_['text_store']     = 'Наши магазины';
+$_['text_contact']   = 'Форма обратной связи';
+$_['text_address']   = 'Адрес';
+$_['text_telephone'] = 'Телефон';
+$_['text_fax']       = 'Факс';
+$_['text_open']      = 'Режим работы';
+$_['text_comment']   = 'Комментарии';
+$_['text_message']   = '<p>Ваш запрос был успешно отправлен!</p>';
 // Entry
-$_['entry_name']     = 'Your Name';
-$_['entry_email']    = 'E-Mail Address';
-$_['entry_enquiry']  = 'Enquiry';
+$_['entry_name']     = 'Ваше имя';
+$_['entry_email']    = 'Адрес эл. почты';
+$_['entry_enquiry']  = 'Запрос';
 // Email
-$_['email_subject']  = 'Enquiry %s';
+$_['email_subject']  = 'Запрос %s';
 // Errors
-$_['error_name']     = 'Name must be between 3 and 32 characters!';
-$_['error_email']    = 'E-Mail Address does not appear to be valid!';
-$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
+$_['error_name']     = 'Имя должно быть от 3 до 32 символов!';
+$_['error_email']    = 'Адрес электронной почты не является действительным!';
+$_['error_enquiry']  = 'Запрос должен быть от 10 до 3000 символов!';
